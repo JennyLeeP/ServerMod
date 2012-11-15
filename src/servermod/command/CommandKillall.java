b591ff26e0087ad7ba4db45ea183408886b85174
richardg867 <richard@viamep.com>
2012-11-15 14:52:55 -0200

    Forgot to add /killall

---
+++ src/servermod/command/CommandKillall.java
@@ -0,0 +1,50 @@
+package servermod.command;
+
+import java.util.List;
+import java.util.Set;
+
+import servermod.core.ServerMod;
+
+import net.minecraft.src.Entity;
+import net.minecraft.src.EntityList;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.ICommandSender;
+import net.minecraft.src.PlayerNotFoundException;
+import net.minecraft.src.World;
+import net.minecraft.src.WrongUsageException;
+
+public class CommandKillall extends Command {
+	public CommandKillall() {
+		super("killall");
+	}
+
+	@Override
+	public void processCommand(ICommandSender var1, String[] var2) {
+		if (var2.length < 1) throw showUsage(var1);
+		
+		String name = null;
+		String pname = func_82360_a(var1, var2, 0);
+		for (String ename : (Set<String>)EntityList.stringToClassMapping.keySet()) {
+			if (ename.equalsIgnoreCase(pname)) name = ename;
+		}
+		
+		if (name == null) throw new PlayerNotFoundException("That entity type is unknown");
+		
+		int removed = 0;
+		World world = var1 instanceof Entity ? ((Entity)var1).worldObj : ServerMod.server.worldServerForDimension(0);
+		for (Entity ent : (List<Entity>)world.loadedEntityList) {
+			String string = EntityList.getEntityString(ent);
+			if (string != null && string.equalsIgnoreCase(name) && !(ent instanceof EntityPlayer)) {
+				ent.setDead();
+				removed++;
+			}
+		}
+		
+		notifyAdmins(var1, "Removed "+removed+" entities of type "+name+" ["+((Class)EntityList.stringToClassMapping.get(name)).getName()+"]");
+	}
+
+	@Override
+	public String getCommandUsage(ICommandSender var1) {
+		return "/"+name+" type";
+	}
+}
