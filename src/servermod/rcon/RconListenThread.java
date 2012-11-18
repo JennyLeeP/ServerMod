b70b322647b0615425d60343fdddf562de5aa19a
richardg867 <richard@viamep.com>
2012-11-18 12:01:14 -0200

    Add improved RCON. Works via telnet, authenticates via MC and uses the op list.
    I honestly don't know how well will mods react to command senders with spaces, but if anything I can change the name.

---
+++ src/servermod/rcon/RconListenThread.java
@@ -0,0 +1,26 @@
+package servermod.rcon;
+
+import java.net.ServerSocket;
+import java.util.logging.Level;
+
+public class RconListenThread extends Thread {
+	private ServerSocket socket;
+	
+	public RconListenThread(ServerSocket socket) {
+		super("ServerMod RCON Listen Thread");
+		
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		while (true) {
+			try {
+				RconClientThread thread = new RconClientThread(socket.accept());
+				thread.start();
+			} catch (Throwable e) {
+				Rcon.instance.log.log(Level.WARNING, "RCON: Accept failed", e);
+			}
+		}
+	}
+}
