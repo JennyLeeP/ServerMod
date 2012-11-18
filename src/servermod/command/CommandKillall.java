2fcf2e9a549b52e5f95a787580efeb82374f2c2e
richardg867 <richard@viamep.com>
2012-11-18 11:38:07 -0200

    Make /killall kill entities in all worlds, thanks ArashiDragon

--- src/servermod/command/CommandKillall.java
+++ src/servermod/command/CommandKillall.java
@@ -31,12 +31,13 @@ public class CommandKillall extends Command {
 		if (name == null) throw new PlayerNotFoundException("That entity type is unknown");
 		
 		int removed = 0;
-		World world = var1 instanceof Entity ? ((Entity)var1).worldObj : ServerMod.server.worldServerForDimension(0);
-		for (Entity ent : (List<Entity>)world.loadedEntityList) {
-			String string = EntityList.getEntityString(ent);
-			if (string != null && string.equalsIgnoreCase(name) && !(ent instanceof EntityPlayer)) {
-				ent.setDead();
-				removed++;
+		for (World world : ServerMod.server.worldServers) {
+			for (Entity ent : (List<Entity>)world.loadedEntityList) {
+				String string = EntityList.getEntityString(ent);
+				if (string != null && string.equalsIgnoreCase(name) && !(ent instanceof EntityPlayer)) {
+					ent.setDead();
+					removed++;
+				}
 			}
 		}
 		
